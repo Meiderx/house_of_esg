07b672f50aece9de3411590be8dcb057b104c93d
Meiderx <124437142+Meiderx@users.noreply.github.com>
2024-07-14 21:12:58 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function showResumo(element) {
+    element.querySelector('.resumo').style.display = 'block';
+}
+
+function hideResumo(element) {
+    element.querySelector('.resumo').style.display = 'none';
+}
+
+function redirectToIntegrante(url) {
+    window.location.href = url;
+}
